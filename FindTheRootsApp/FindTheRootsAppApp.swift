960d1e447857f4430cdeb5642fa07fdb8dd70f38
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-14 11:07:18 -0500

    Initial Commit

---
+++ FindTheRootsApp/FindTheRootsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsAppApp.swift
+//  FindTheRootsApp
+//
+//  Created by Ahmet Kaan Caliskan on 2025-01-14.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
