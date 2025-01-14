71bd61cb9df32dcff79a0e1ec4281f37d40dc52c
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-14 11:52:53 -0500

    Finished making the static interface.

--- FindTheRootsApp/RootsCalculatorView.swift
+++ FindTheRootsApp/RootsCalculatorView.swift
@@ -8,13 +8,59 @@
 import SwiftUI
 
 struct RootsCalculatorView: View {
+    @State var currentnumber: Double = 1
     var body: some View {
         NavigationStack{
-            navigationTitle("Find The Roots")
-            VStack {
+            VStack{
                 
+                Image("QuadraticFormula")
+                    .resizable()
+                    .scaledToFit()
+                Image("StandardForm")
+                    .resizable()
+                    .scaledToFit()
+                HStack{
+                    Text("a:1.0")
+                        .padding()
+                        .font(Font.custom("Times New Roman",
+                                          size: 24.0,
+                                          relativeTo: .body))
+                    Text("b:-6.0")
+                        .padding()
+                        .font(Font.custom("Times New Roman",
+                                          size: 24.0,
+                                          relativeTo: .body))
+                    Text("c:8.0")
+                        .font(Font.custom("Times New Roman",
+                                          size: 24.0,
+                                          relativeTo: .body))
+                }
+                VStack{
+                    
+                    HStack{
+                        Slider(
+                            value: $currentnumber,
+                            in: 0...100,
+                            step: 1
+                        )
+                        Slider(
+                            value: $currentnumber,
+                            in: 0...100,
+                            step: 1
+                        )
+                        Slider(
+                            value: $currentnumber,
+                            in: 0...100,
+                            step: 1
+                        )
+                    }
+                    Text("x≈2.00 and x≈4.00")
+                }
+                .navigationTitle("Find The Roots")
             }
             .padding()
+            Spacer()
+            
         }
     }
 }
